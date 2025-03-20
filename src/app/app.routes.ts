78c903e5a2f42e2d66baa606f9584b9d64540b35
Felipe de Oliveira Fernandes <felipefoz10@hotmail.com>
2025-03-20 17:01:14 -0300

    add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/layout/login/login.component';
+import { PrincipalComponent } from './components/layout/principal/principal.component';
+import { AlunosListComponent } from './components/aluno/alunos-list/alunos-list.component';
+import { MenuComponent } from './components/layout/menu/menu.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {path: "", redirectTo: "login", pathMatch: 'full'},
+    {path: "login", component: LoginComponent},
+    {path: "menu", component: MenuComponent},
+    {path: "principal", component: PrincipalComponent, children:[
+        {path: "alunos", component: AlunosListComponent},
+        {path: "alunos/new", component: AlunosListComponent}
+    ]}
+];
