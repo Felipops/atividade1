79efe4b7fbce4dd85a1b6269ba028383d4d68318
Felipe de Oliveira Fernandes <felipefoz10@hotmail.com>
2025-03-21 22:19:35 -0300

    add forms

--- src/app/components/aluno/alunos-form/alunos-form.component.ts
+++ src/app/components/aluno/alunos-form/alunos-form.component.ts
@@ -1,12 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
+import { Aluno } from '../../../models/aluno';
+import { FormsModule } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-alunos-form',
   standalone: true,
-  imports: [],
+  imports: [MdbFormsModule, FormsModule],
   templateUrl: './alunos-form.component.html',
   styleUrl: './alunos-form.component.scss'
 })
 export class AlunosFormComponent {
+
+
+  aluno: Aluno = new Aluno();
+
+  rotaAtivida = inject(ActivatedRoute);
+
+  constructor() {
+    let id = this.rotaAtivida.snapshot.params['id'];
+    if (id) {
+      //AQUI VC VAI CHAMAR O FINDBYID()
+      let aluno1 = new Aluno();
+      aluno1.id = 1;
+      aluno1.nome = 'HRV';
+      aluno1.cpf = 'HONDA';
+      aluno1.telefone = '999999999';
+      this.aluno = aluno1; //setar o objeto no carro do formuljario
+    }
+  }
+
+  save() {
+    if (this.aluno.id > 0) {
+      // UPDATE
+      alert('estou fazendo um update....');
+    } else {
+      // SAVE
+      alert('estou fazendo um save');
+    }
+  }
 
 }
