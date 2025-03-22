2a9ab6106983766b20622eb5771565309d010b08
Felipe Fernandes <felipefoz10@hotmail.com>
2025-03-22 14:29:52 -0300

    feito

--- src/app/components/curso/cursos-list/cursos-list.component.ts
+++ src/app/components/curso/cursos-list/cursos-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Curso } from '../../../models/curso';
 
 @Component({
   selector: 'app-cursos-list',
@@ -8,5 +9,40 @@ import { Component } from '@angular/core';
   styleUrl: './cursos-list.component.scss'
 })
 export class CursosListComponent {
+lista: Curso[] = [];
+  
+    constructor() {
+      this.findAll();
+    }
+  
+  
+    findAll(){
+      //DEPOIS EU VOU TER A COMUNICAÇÃO COM O SERVICE 
+  
+      let curso1 = new Curso();
+      curso1.id = 1;
+      curso1.nome = 'ADS';
+      
 
+  
+      let curso2 = new Curso();
+      curso2.id = 2;
+      curso2.nome = 'MATEMATICA';
+      
+  
+      let curso3 = new Curso();
+      curso3.id = 3;
+      curso3.nome = 'fisica';
+    
+  
+      this.lista.push(curso1, curso2, curso3);
+    
+    }
+  
+    delete(curso : Curso){
+      let indice = this.lista.findIndex(x => {return x.id == curso.id});
+      if(confirm('Deseja deletar isso aí?')){
+        this.lista.splice(indice, 1); //deletando um objeto na posição INDICE
+      }
+    }
 }
