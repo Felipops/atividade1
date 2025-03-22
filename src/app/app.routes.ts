b71e05ad3c952b0cbcd2a5c7f684ffff9725374f
Felipe Fernandes <felipefoz10@hotmail.com>
2025-03-22 14:31:52 -0300

    finalizado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,6 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './components/layout/login/login.component';
 import { PrincipalComponent } from './components/layout/principal/principal.component';
 import { AlunosListComponent } from './components/aluno/alunos-list/alunos-list.component';
-import { MenuComponent } from './components/layout/menu/menu.component';
 import { AlunosFormComponent } from './components/aluno/alunos-form/alunos-form.component';
 import { ProfessoresListComponent } from './components/professor/professores-list/professores-list.component';
 import { ProfessoresFormComponent } from './components/professor/professores-form/professores-form.component';
@@ -19,9 +18,9 @@ export const routes: Routes = [
         {path: "alunos", component: AlunosListComponent},
         {path: "alunos/new", component: AlunosFormComponent},
         {path: "alunos/edit/:id", component: AlunosFormComponent},
-        {path: "professor", component: ProfessoresListComponent},
-        {path: "professor/new", component: ProfessoresFormComponent},
-        {path: "professor/edit/:id", component: ProfessoresFormComponent},
+        {path: "professores", component: ProfessoresListComponent},
+        {path: "professores/new", component: ProfessoresFormComponent},
+        {path: "professores/edit/:id", component: ProfessoresFormComponent},
         {path: "cursos", component: CursosListComponent},
         {path: "cursos/new", component: CursosFormComponent},
         {path: "cursos/edit/:id", component: CursosFormComponent},
